f88701886de51bd903079bc1c91e119e6693a921
corinaemenuge <corinaemenuge@me.com>
2016-04-26 04:28:47 -0500

    Final Final

--- Final Project/Site/js/main.js
+++ Final Project/Site/js/main.js
@@ -7,3 +7,35 @@ $(".js-close").on("click", function(e){
 	e.preventDefault();
 	$("body").removeClass("active");
 });
+
+
+$(document).ready(function(){
+
+$('.HowardButton').on('click', function(){
+	console.log("clicked function working");
+
+
+	overlay();
+});
+
+function overlay() {
+	// var el = document.getElementsByClassName("HowardOverlay");
+	$(".HowardOverlay").css('visibility', 'visible');
+	// el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+
+	console.log("clicked");
+}
+
+$('.CloseHoward').on('click', function(){
+
+	hideoverlay();
+});
+
+});
+
+
+
+function hideoverlay() {
+	
+	$(".HowardOverlay").css('visibility', 'hidden');
+}
